e0b81ef4a9868c35e42b65f39692f570e86efaa4
Lu Ting <chialutingz@gmail.com>
2023-08-07 14:53:47 +0800

    Added Todo components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Todo from './components/Todo';
+
 function App() {
-  return <div>Hello!</div>
+  return <div>
+    <h1>My Todos</h1>
+    <Todo text='Learn React'/>
+    <Todo text='Master React'/>
+    <Todo text='Full React Course'/>
+  </div>;
 }
 
 export default App;
